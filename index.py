be0933b6bc4881cf69e66750cf1b8909cc606ecc
Ilan Wanounou <wanounouilan@gmail.com>
2023-04-06 09:18:15 +0200

    Chargement des donées user

--- index.py
+++ index.py
@@ -1,6 +1,15 @@
 import keyboard
+import json
+def loadData():
+    try:
+        with open("data.json") as json_file:
+            data = json.load(json_file)
+            return data
+    except:
+        return {}
 
 def on_key_press(key):
     print(f"Touche pressée : {key.name}")
 
 keyboard.on_press(on_key_press)
+data = loadData()
