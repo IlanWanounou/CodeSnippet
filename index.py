a9f4f2405786ea47537dc15f910305b6cdab0086
Ilan Wanounou <wanounouilan@gmail.com>
2023-04-06 09:54:39 +0200

    ajout méthode pour check les entrée user

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 import keyboard
 import json
 import time
+from plyer import notification
+
 def loadData():
     try:
         with open("data.json") as json_file:
@@ -9,10 +11,31 @@ def loadData():
     except:
         return {}
 
-def on_key_press(key):
-    print(f"Touche pressée : {key.name}")
+def checkCode(code):
+    global str
+    if code in data.values():
+        str = ""
+        notification.notify(
+            title = "Code correct",
+            message = "Code correct",
+            timeout = 5
+        )
+    else:
+        if(len(code) > 4):
+            notification.notify(
+                title = "Code incorrect",
+                message = "Code incorrect",
+                timeout = 5
+        )
 
+def on_key_press(key):
+    global str
+    str += key.name
+    checkCode(str)
+
+str = ""
 keyboard.on_press(on_key_press)
 data = loadData()
+
 while True:
     time.sleep(1)
