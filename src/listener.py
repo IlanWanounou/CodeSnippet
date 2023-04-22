94e5eaaec39179ee6179c1638fbffc1b2b97a5ea
Ilan Wanounou <wanounouilan@gmail.com>
2023-04-22 03:36:47 +0200

    file fixed pylint

--- src/listener.py
+++ src/listener.py
@@ -1,20 +1,24 @@
+"""Listen for key presses and replace them with the corresponding value"""
 import threading
 import re
 from pynput import keyboard
 from src import utils
 
 def check_code():
-    if next((key for key in data.keys() if re.search(key, on_press.key_sequence)), None):
-        return True, next((key for key in data.keys() if re.search(key, on_press.key_sequence)), None)
-    else:
-        return False, None
+    """Check if the key sequence is in the data dictionary and return the corresponding value"""
+    key_match = next((key for key in data.keys() if re.search(key, on_press.key_sequence)), None)
+    if key_match:
+        return True, key_match
+    return False, None
 
 def clear():
+    """Clear the key sequence"""
     on_press.key_sequence = ""
-    return
 
 def on_press(key):
-    global data
+    """
+    Check if the key sequence is in the data dictionary and replace it with the corresponding value
+    """
     try:
         if key == keyboard.Key.backspace:
             on_press.key_sequence = on_press.key_sequence[:-1]
@@ -28,14 +32,12 @@ def on_press(key):
             on_press.key_sequence = ""
             keyboard.Controller().type(data[key])
             threading.Timer(0.1, clear).start()
-            
     except AttributeError:
         pass
 on_press.key_sequence = ""
 data = utils.load_data()
 
-
 def start_listener():
+    """Start the listener"""
     with keyboard.Listener(on_press=on_press) as listener:
         listener.join()
-
